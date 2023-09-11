5c0b3f5b012eff046281c6c8d7835ed314a9d075
shaheemMPM <shaheem.mpm@gmail.com>
2023-09-11 18:33:53 +0530

    Render template from flask

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 
 pygame.mixer.init()
 
-api_base_url = 'http://127.0.0.1:5000/'
+api_base_url = 'http://127.0.0.1:5000/next-song'
 
 while True:
 	response = requests.get(api_base_url)
--- server.py
+++ server.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def dashboard():
+  return render_template('index.html')
+
+@app.route('/next-song', methods=['GET'])
+def next_song():
   return '1'
 
 if __name__ == '__main__':
